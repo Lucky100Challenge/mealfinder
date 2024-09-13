5165d342881298abda34a87914dc8a21aa8fff70
Mike Decavalcante <thatnotiondude@gmail.com>
2024-09-13 12:23:22 -0500

    Update AboutSection.tsx

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -3,7 +3,12 @@ const AboutSection = () => {
     <div className="bg-white shadow-md rounded-lg p-6 mb-8">
       <h2 className="text-3xl font-bold mb-4">About Recipe Planner</h2>
       <p className="text-gray-700">
-        Welcome to Recipe Planner, your ultimate destination for discovering delicious meals and planning your weekly menus effortlessly. Powered by TheMealDB API, our platform allows you to search for recipes by name or main ingredient, explore a wide variety of meal categories, and even get inspired with random meal suggestions. Whether you're a seasoned chef or a cooking enthusiast, Recipe Planner makes it easy to find and organize your favorite dishes. Enjoy a modern and sleek interface designed with Tailwind CSS and Next.js for a seamless user experience.
+        Welcome to Recipe Planner, your ultimate destination for discovering delicious meals and planning your weekly menus effortlessly. 
+        Powered by TheMealDB API, our platform allows you to search for recipes by name or main ingredient, explore a wide variety of meal categories, 
+        and even get inspired with random meal suggestions. Whether you're a seasoned chef or a cooking enthusiast, 
+        Recipe Planner makes it easy to find and organize your favorite dishes. 
+        
+        Enjoy a modern and sleek interface designed with Tailwind CSS and Next.js for a seamless user experience.
       </p>
     </div>
   )
