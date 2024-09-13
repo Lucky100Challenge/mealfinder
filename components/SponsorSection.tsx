04a710466f5c7840ef6870d852a4ea07dbf648c5
Mike Decavalcante <thatnotiondude@gmail.com>
2024-09-13 14:03:17 -0500

    sponsors

--- components/SponsorSection.tsx
+++ components/SponsorSection.tsx
@@ -1,23 +1,31 @@
 import Image from 'next/image'
+import zSponsor from '../images/z.jpg'
+import jansSponsor from '../images/jans.jpg'
 
 const sponsors = [
-  '/z.jpg',
-  '/sponsors/sponsor2.png',
-  '/sponsors/sponsor3.png',
+  { src: zSponsor, alt: 'Z Sponsor' },
+  { src: jansSponsor, alt: 'Jans Sponsor' },
   // Add more sponsor images
 ]
 
 const SponsorSection = () => {
   return (
     <div className="p-8">
-      <h2 className="text-2xl mb-4">Our Sponsors</h2>
+      <h2 className="text-2xl mb-4">Challenge Sponsors</h2>
       <div className="flex space-x-4">
-        {sponsors.map((src, index) => (
-          <Image key={index} src={src} alt={`Sponsor ${index + 1}`} width={100} height={100} />
+        {sponsors.map((sponsor, index) => (
+          <Image 
+            key={index} 
+            src={sponsor.src}
+            alt={sponsor.alt}
+            width={100} 
+            height={100} 
+            className="rounded-full"
+          />
         ))}
       </div>
       <div className="mt-8 text-center">
-        <a href="https://example.com/support" className="text-blue-500 hover:underline">
+        <a href="https://buymeacoffee.com/thatsaasdude" className="text-blue-500 hover:underline">
           Want your image here? Support the challenge
         </a>
       </div>
